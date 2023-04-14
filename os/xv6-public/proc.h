1519b3fa1950c3690813e9d8781960b74382896a
yeonic <bellopenerr@gmail.com>
2023-04-14 16:03:59 +0000

    proc.h: Add new memeber for mlfq

--- os/xv6-public/proc.h
+++ os/xv6-public/proc.h
@@ -49,6 +49,13 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+
+  struct {
+    int queuedtick;
+    int priority;
+    int rmtime;
+    int level;
+  } mlfq;
 };
 
 // Process memory is laid out contiguously, low addresses first:
